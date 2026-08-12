4d35a386d118b8d55f5f120e64d47c1b459e8d59
Your Name <you@example.com>
2026-08-12 15:55:08 +0700

    Phase 13.6: Test DMIX bug with AUDIODEV=hw:0,0

--- src/AMTube.cpp
+++ src/AMTube.cpp
@@ -345,11 +345,11 @@ int main(int argc, char* args[]) {
                                     std::string vid_url = (nl != std::string::npos) ? raw_url.substr(0, nl) : raw_url;
                                     std::string aud_url = (nl != std::string::npos) ? raw_url.substr(nl+1) : "";
                                     
-                                    // Phase 13.5 (Test 2): XRUN Deadlock Test (Local File)
+                                    // Phase 13.6 (Test 3): Thuyết DMIX Bug
                                     // system("systemctl stop pulseaudio; amixer -q set Master,0 100% unmute");
 
-                                    // Ép ffplay phát file nội bộ thay vì URL mạng để cắt đứt độ trễ mạng
-                                    std::string cmd="SDL_AUDIODRIVER=alsa ffplay -fs -autoexit test.mp4 >> AMTube_Log.txt 2>&1 &";
+                                    // Ép AUDIODEV=hw:0,0 để SDL cắm thẳng vào mạch điện, né plugin trộn âm dmix rác
+                                    std::string cmd="AUDIODEV=hw:0,0 SDL_AUDIODRIVER=alsa ffplay -fs -autoexit '"+vid_url+"' >> AMTube_Log.txt 2>&1 &";
                                     system(cmd.c_str());
 
                                     // AMTube ở lại chạy nền để gác cửa (Watchdog) chờ phím B
